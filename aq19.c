f1ff5ced40eeee703aff6508f31e48539b9bb682
tejaswinizala <tejaswinipzala@gmail.com>
2025-09-07 11:04:54 +0530

    Delete aq19.c

--- aq19.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main(){
-     // Q19: Area of a circle (π = 22/7)
-    float radius, area;
-    printf("Enter radius of circle: ");
-    scanf("%f", &radius);
-    area = (22 * radius * radius) / 7;
-    printf("Area = %.2f\n", area);
-}
