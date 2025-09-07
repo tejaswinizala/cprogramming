c94a7c154134a5e6d575fcd8e1347ac61d6b8aba
tejaswinizala <tejaswinipzala@gmail.com>
2025-09-07 11:04:25 +0530

    Delete aq17.c

--- aq17.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main(){
-    // Q17: Area and perimeter of a square
-    int side;
-    printf("Enter side of square: ");
-    scanf("%d", &side);
-    printf("Area = %d\n", side * side);
-    printf("Perimeter = %d\n", 4 * side);
-}
