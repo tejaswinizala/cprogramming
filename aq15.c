862e6337739122844cf32b32053af9938526b568
tejaswinizala <tejaswinipzala@gmail.com>
2025-09-07 11:04:00 +0530

    Delete aq15.c

--- aq15.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-void main(){
-     // Q15: Fahrenheit to Celsius
-    printf("Enter Fahrenheit: ");
-    scanf("%f", &fahrenheit);
-    celsius = (5.0 / 9.0) * (fahrenheit - 32);
-    printf("Celsius = %.2f\n", celsius);
-}
