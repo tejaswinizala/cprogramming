d4ca3179b69b25b9b008e7c1e92c8a4f1765a487
tejaswinizala <tejaswinipzala@gmail.com>
2025-09-07 11:05:19 +0530

    Delete aq20.c

--- aq20.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-void main(){
-     float base, height;
-    printf("Enter base and height of triangle: ");
-    scanf("%f%f", &base, &height);
-    area = (base * height) / 2;
-    printf("Area = %.2f\n", area);
-}
