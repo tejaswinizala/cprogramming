120a78e64dfbb201ace722cbaf05a7c09400e434
tejaswinizala <tejaswinipzala@gmail.com>
2025-09-07 11:04:13 +0530

    Delete aq16.c

--- aq16.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main(){
-     // Q16: Simple Interest
-    float p, r, n, si;
-    printf("Enter principal, rate, and time: ");
-    scanf("%f%f%f", &p, &r, &n);
-    si = (p * r * n) / 100;
-    printf("Simple Interest = %.2f\n", si);
-}
