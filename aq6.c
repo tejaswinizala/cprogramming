9bd0f133e77906146feb83788fa4897a1ea13a0b
tejaswinizala <tejaswinipzala@gmail.com>
2025-09-07 11:06:10 +0530

    Delete aq6.c

--- aq6.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main(){
-
-// Q6: Convert hours to minutes
-    int hours;
-    printf("Enter hours: ");
-    scanf("%d", &hours);
-    printf("Minutes = %d\n", hours * 60);
-}
